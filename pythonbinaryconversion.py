268946e9666710c0785c239c6b626f57d1cff303
Alan Sanchez <asanch317@west-mec.org>
2023-09-21 18:16:26 -0700

    Update pythonbinaryconversion.py
    Binary portion finished

--- pythonbinaryconversion.py
+++ pythonbinaryconversion.py
@@ -1,3 +1,12 @@
 import math
 
-#hello hai
+def decimalToBinary(n):
+    return "{0:b}".format(int(n))
+   
+dTb = int(input("What is your number?"))
+
+
+dTb = str(dTb)
+print("The Binary of " + dTb + " is:")
+dTb = int(dTb)
+print(decimalToBinary(dTb))
